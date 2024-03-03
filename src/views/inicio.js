5f9793f2b549418dac1a1f8c0887b5e926b9e7ad
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-03-03 16:23:20 -0400

    Update inicio.js

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -7,7 +7,7 @@ import { Helmet } from 'react-helmet'
 import SectionNumeral from '../components/section-numeral'
 import BlogPostCard2 from '../components/blog-post-card2'
 import './inicio.css'
-import '../../connect.js'
+import '../connect.js'
 import '../components/script.js'
 
 const Inicio = (props) => {
