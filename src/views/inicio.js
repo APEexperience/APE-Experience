02293e3c55d696a91c45c6ffd16d54bf7fd22788
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-03-03 16:13:58 -0400

    Update inicio.js

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -8,8 +8,8 @@ import SectionNumeral from '../components/section-numeral'
 import BlogPostCard2 from '../components/blog-post-card2'
 import './inicio.css'
 import '../../connect.js'
+import '../newsletter/script.js'
 
-const mongoose = require('mongoose');
 const Inicio = (props) => {
   return (
     <div className="inicio-container">
@@ -634,36 +634,6 @@ const Inicio = (props) => {
             </button>
           </div>
         </div>
-		      <script>
-$(document).ready(function() {
-  $('.inicio-button2').on('click', function(event) {
-    event.preventDefault();
-
-    let nombre = $('.inicio-textinput').val();
-    let correo = $('.inicio-textinput1').val();
-
-    // Enviar datos al servidor
-    $.ajax({
-      type: 'POST',
-      url: '/inicio.js',
-      data: {nombre: nombre, correo: correo},
-      success: function(response) {
-        // Manejo de la respuesta del servidor
-        console.log(response);
-        // Por ejemplo, mostrar un mensaje de éxito al usuario
-        alert('¡Gracias por suscribirte al newsletter!');
-      },
-      error: function(error) {
-        // Manejo de errores
-        console.error('Error al suscribirse: ', error);
-        // Por ejemplo, mostrar un mensaje de error al usuario
-        alert('Ocurrió un error al suscribirte al newsletter. Por favor, inténtalo de nuevo.');
-      }
-    });
-  });
-});
-
-		      </script>
       </section>
       <footer className="inicio-footer">
         <div className="inicio-content6">
