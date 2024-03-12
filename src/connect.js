322340e50e40914d06374b391537df49087eae21
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-03-11 20:34:11 -0400

    Update connect.js

--- src/connect.js
+++ src/connect.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://cloudflare.com/');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
