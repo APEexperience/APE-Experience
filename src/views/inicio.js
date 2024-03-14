50ca1f3a0b2304fbf2263e0026efd31329610a87
APE Experience <151045379+APEexperience@users.noreply.github.com>
2024-03-14 15:55:09 -0400

    Update inicio.js

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -25,7 +25,7 @@ const handleSubmit = async (event) => {
   event.preventDefault();
 
   try {
-    const response = await fetch('/api/suscripciones', {
+    const response = await fetch('https://ape.sqlmyadmin.workers.dev/api/suscripciones', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
